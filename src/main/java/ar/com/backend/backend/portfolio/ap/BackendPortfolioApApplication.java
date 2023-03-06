0272bdea8092f45786eaad240abe7f586b14fdd0
Eduardo Cabral <eduardomcabral1974@gmail.com>
2023-03-06 18:21:18 -0300

    primer commit

---
+++ src/main/java/ar/com/backend/backend/portfolio/ap/BackendPortfolioApApplication.java
@@ -0,0 +1,13 @@
+package ar.com.backend.backend.portfolio.ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPortfolioApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPortfolioApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/backend/backend/portfolio/ap/BackendPortfolioApApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.backend.backend.portfolio.ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPortfolioApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
